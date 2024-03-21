5e475b74d6554b2f97296369831cbefc1445b670
iseki <admin@iseki.space>
2024-03-22 01:11:17 +0800

    test: fix testcases

--- src/test/kotlin/space/iseki/cmdpipe/CmdTest.kt
+++ src/test/kotlin/space/iseki/cmdpipe/CmdTest.kt
@@ -2,24 +2,26 @@ package space.iseki.cmdpipe
 
 import org.junit.jupiter.api.Test
 import java.nio.charset.Charset
+import kotlin.test.assertTrue
 
 class CmdTest {
     private val isWindows = System.getProperty("os.name").startsWith("Windows")
-    private val cmd3 = if (isWindows) listOf("cmd", "/c", "dir /?a") else listOf("ls", "-aa")
+    private val cmdError = if (isWindows) listOf("cmd", "/c", "dir /xxxx") else listOf("ls", "--xxxx")
     private val cmd = if (isWindows) listOf("cmd", "/c", "dir") else listOf("ls")
 
     @Test
-    fun test1() {
+    fun testErrorCmd() {
         val stdout = Cmd.read { ctx ->
             ctx.stream.bufferedReader(Charset.defaultCharset()).readText().prependIndent("> ")
         }
         val stderr = Cmd.read { ctx ->
             ctx.stream.bufferedReader(Charset.defaultCharset()).readText().prependIndent("stderr> ")
         }
-        Cmd.Builder().cmdline(cmd3).handleStdout(stdout).handleStderr(stderr).start().use {
+        Cmd.Builder().cmdline(cmdError).handleStdout(stdout).handleStderr(stderr).start().use {
             println(stdout.future().get())
             println(stderr.future().get())
         }
+        assertTrue { stderr.future().get().contains("xxxx") }
     }
 
     @Test
