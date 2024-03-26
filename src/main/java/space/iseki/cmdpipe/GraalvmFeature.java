f38eb4a2645db79663a54c4fc44bca4e0731353d
iseki <admin@iseki.space>
2024-03-26 18:56:07 +0800

    fix: package-private GraalvmFeature.java

--- src/main/java/space/iseki/cmdpipe/GraalvmFeature.java
+++ src/main/java/space/iseki/cmdpipe/GraalvmFeature.java
@@ -4,12 +4,11 @@ import org.graalvm.nativeimage.hosted.Feature;
 import org.graalvm.nativeimage.hosted.RuntimeResourceAccess;
 
 @SuppressWarnings("unused")
-public class GraalvmFeature implements Feature {
+class GraalvmFeature implements Feature {
     @Override
     public void duringSetup(DuringSetupAccess access) {
         var unnamedModule = this.getClass().getClassLoader().getUnnamedModule();
         RuntimeResourceAccess.addResource(unnamedModule, "space/iseki/cmdpipe/TextualFormatter.properties");
         RuntimeResourceAccess.addResource(unnamedModule, "space/iseki/cmdpipe/TextualFormatter_zh.properties");
     }
-
 }
